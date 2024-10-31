9472c310d592d309d73e2bcbb32dde636241010a
Medkelawit_Abadina <>
2024-10-31 17:05:29 +0300

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -33,9 +34,10 @@ function App() {
       </p>
       
       <WelcomeMessage/>
-      <Header />
+      <Header/>
       <MainContent />
-      <Footer />
+      <Footer/>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       
     </>
   )
