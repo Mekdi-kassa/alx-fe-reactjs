5bf6875a67ab99d8d9438b4a8c215fac9fd86991
Medkelawit_Abadina <>
2024-10-31 16:47:16 +0300

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './WelcomeMessage'
 import WelcomeMessage from './WelcomeMessage'
 
 function App() {
