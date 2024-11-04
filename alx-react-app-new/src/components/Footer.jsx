486788889c25902c34044838d854208a59526831
Medkelawit_Abadina <>
2024-11-05 00:03:06 +0300

    add

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer style={{border:'2px',backgroundColor:'pink',padding:'20px'}}>
+            <p style={{ color:"white",fontWeight:'lighter'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    const s={color:'pink',backgroundColor:'#000'}
+    return(
+        <main style={{color:'palegreen',fontFamily:'sans-serif',border:'20px',borderRadius:'5px',borderStyle:'dashed'}}>
+            <p style={s}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+export  default MainContent;
