a27448e3708a08f1ef3e766c64b799994c90e93b
Medkelawit_Abadina <>
2024-11-07 22:48:37 +0300

    add

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,5 @@
+import UserInfo from "./UserInfo";
+function ProfilePage(){
+    return<UserInfo userData={userData}/>
+}
+export default ProfilePage;
---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,5 @@
+import UserDetails from "./UserDetails";
+function UserInfo(){
+    return< UserDetails userData={userData} />
+}
+export default UserInfo;
