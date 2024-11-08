26dd92c0e7618ddbe6b643d63a4d076c52ec7feb
Medkelawit_Abadina <>
2024-11-08 16:10:11 +0300

    add

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -11,6 +11,7 @@ import ProfilePage from './ProfilePage'
 import UserContext from './UserContext'
 function App() {
   const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <>
       <div>
@@ -39,7 +40,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <UserContext.Provider>
+      <UserContext.Provider  value={userData}>
         <ProfilePage />;
       </UserContext.Provider>
       
