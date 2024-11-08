a7fc5e05cb8cdaaa8afcc2f89865a793a5bc4240
Medkelawit_Abadina <>
2024-11-08 16:32:01 +0300

    add

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <nav style={{backgroundColor:'#333',padding:'10px'}}>
-            <ul style={{listStyle:'none',direction:'flex',justifyContent:'space-around'}}>
+            <ul style={{listStyle:'none',display:'flex',justifyContent:'space-around'}}>
                 <li>
                 <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
                 </li>
