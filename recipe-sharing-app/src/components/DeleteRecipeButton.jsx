0c74323bcee9b6fbdc9655828c5452f507df01af
Medkelawit_Abadina <>
2024-11-17 19:02:05 +0300

    add

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,17 @@
-// src/components/DeleteRecipeButton.js
+// src/components/DeleteRecipeButton.jsx
 import React from 'react';
-import { useRecipeStore } from '../store/recipeStore';
+import useRecipeStore from '../components/recipeStore';
+import { useNavigate } from 'react-router-dom';
 
-const DeleteRecipeButton = ({ recipeId }) => {
+
+const DeleteRecipeButton = ({ recipeId, onDelete }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
+
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/'); // Navigate back to the home page after deletion
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
