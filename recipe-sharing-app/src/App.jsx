5c1a6b1bcbe1118716b974fdf9e6da7f25685039
Medkelawit_Abadina <>
2024-11-17 17:50:03 +0300

    add

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RecipeList from './Components/RecipesList';
+import RecipeList from './Components/RecipeList';
 import AddRecipeForm from './Components/AddRecipeForm';
 
 const App = () => {
