0949587d1ad447ddb2375d7af90d38cb469bbd7e
Medkelawit_Abadina <>
2024-12-08 22:46:11 +0300

    add

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -7,3 +7,10 @@ export default {
   plugins: [],
 }
 
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
