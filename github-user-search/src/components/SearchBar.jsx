a4760e9fe5de9cfae59c305e79c51330e34ca2c6
Medkelawit_Abadina <>
2024-12-15 23:36:30 +0300

    add

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username.trim() !== '') {
+      onSearch(username); // Trigger the search
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} // Update state with input value
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ github-user-search/src/services/githubService.jsx
@@ -0,0 +1,14 @@
+// src/services/githubService.js
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com/users';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
+    return response.data; // Return the user data
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+};
